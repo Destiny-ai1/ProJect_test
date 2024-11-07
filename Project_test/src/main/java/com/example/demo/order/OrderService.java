9156172d9042ac40a10cbdbb2cc5bfb15296536d
Chris <chrisjun312@naver.com>
2024-11-07 17:17:47 +0900

    JobFailException추가 Order부분 추가

--- Project_test/src/main/java/com/example/demo/order/OrderService.java
+++ Project_test/src/main/java/com/example/demo/order/OrderService.java
@@ -1,5 +1,42 @@
 package com.example.demo.order;
 
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.exception.JobFailException;
+
+@Service
 public class OrderService {
 
+    @Autowired
+    private OrderDao orderDao;
+
+    public Long createOrder(OrderDto.Create dto) {
+        Order order = dto.toEntity();
+        orderDao.save(order);
+        return order.getOrderNo();
+    }
+
+    public OrderDto.Read getOrder(Long orderNo) {
+        return orderDao.findById(orderNo)
+                .orElseThrow(() -> new JobFailException("주문을 찾을 수 없습니다"));
+    }
+
+    public List<OrderDto.OrderList> getAllOrders() {
+        return orderDao.findAll();
+    }
+
+    public void updateOrder(OrderDto.Update dto) {
+        OrderDto.Read order = orderDao.findById(dto.getOrderNo())
+                .orElseThrow(() -> new JobFailException("주문을 찾을 수 없습니다"));
+        orderDao.update(dto.toEntity());
+    }
+
+    public void deleteOrder(Long orderNo) {
+        OrderDto.Read order = orderDao.findById(orderNo)
+                .orElseThrow(() -> new JobFailException("주문을 찾을 수 없습니다"));
+        orderDao.delete(orderNo);
+    }
 }
