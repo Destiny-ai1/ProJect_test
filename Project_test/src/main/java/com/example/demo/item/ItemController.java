a334df6b53060fa4417a4d2e45d7072e73c2b31c
한승민 <gkstmddk131@naver.com>
2024-11-07 15:50:40 +0900

    item insert(test 확인), category-mapper/product-mapper수정, sql테이블 item부분 수정

--- Project_test/src/main/java/com/example/demo/item/ItemController.java
+++ Project_test/src/main/java/com/example/demo/item/ItemController.java
@@ -1,5 +1,22 @@
 package com.example.demo.item;
 
-public class ItemController {
+import java.security.Principal;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ItemController {
+	@Autowired
+	private ItemService service;
+	
+	@GetMapping("/")
+	public ModelAndView list(Principal p) {
+		List<ItemDto.ItemList> result = service.findAll();
+		return new ModelAndView("item/list").addObject("result", result);
+		
 	}
+}
--- Project_test/src/main/java/com/example/demo/item/ItemService.java
+++ Project_test/src/main/java/com/example/demo/item/ItemService.java
@@ -1,5 +1,26 @@
 package com.example.demo.item;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.demo.category.CategoryDao;
+import com.example.demo.item.ItemDto.ItemList;
+
 public class ItemService {
+	@Autowired
+	private ItemDao itemDao;
+	@Autowired
+	private CategoryDao categoryDao;
+	
+	public List<Map> findMajorCategory() {
+		return categoryDao.findMajorCategory();
+	}
+
+	public List<ItemList> findAll() {
+		return itemDao.findall(/*잠시보류*/);
+	}
+	
 	
 }
