5906a497ed8498a8efeab473cc040b13109cabd8
kiwon <bob3824@naver.com>
2024-12-02 14:49:29 +0900

    복구완료

--- Project_test/src/main/resources/static/script/input.js
+++ Project_test/src/main/resources/static/script/input.js
@@ -1,4 +1,21 @@
-// 공백 및 패턴 체크 함수
+//회원가입관련
+function loadProfile() {
+    const file = $('#profile')[0].files[0];
+    const maxSize = 1024 * 1024;
+    if(file.size > maxSize) {
+        alert("프사는 최대 1MB입니다");
+        $('#profile').val("");
+        return false;
+    }
+    
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function(e) {
+        $('#show-profile').attr("src", e.target.result);
+    }
+    return true;
+}
+
 function emptyAndPatternCheck(입력값, $출력할곳, pattern, emptyErrorMessage, patternErrorMessage) {
     $출력할곳.text('');
     if(입력값 == '') {
@@ -19,26 +36,14 @@ function nameCheck() {
     return emptyAndPatternCheck(value, $nameMsg, pattern, '이름을 입력하세요', '이름은 최소 2글자 이상이어야 합니다');
 }
 
-// 전화번호 자동 포맷 함수
-function formatPhoneInput(event) {
-    let value = event.target.value.replace(/\D/g, ''); // 숫자 이외의 문자 제거
-    if (value.length > 3 && value.length <= 7) {
-        value = value.replace(/(\d{3})(\d+)/, '$1-$2');
-    } else if (value.length > 7) {
-        value = value.replace(/(\d{3})(\d{3,4})(\d{4})/, '$1-$2-$3');
-    }
-    event.target.value = value; // 전화번호 입력 필드 값 갱신
-}
-
 // 전화번호 검증 함수
 function phoneCheck() {
-    const pattern = /^\d{3}-\d{3,4}-\d{4}$/;  // 010-0000-0000 형식
+    const pattern = /^[0-9]{10,11}$/;  // 10자리 또는 11자리 숫자
     const value = $('#phone').val();
     const $phoneMsg = $('#phone-msg');
     return emptyAndPatternCheck(value, $phoneMsg, pattern, '전화번호를 입력하세요', '유효한 전화번호 형식이 아닙니다');
 }
 
-// 생일 검증 함수
 function birthdayCheck() {
     const pattern = /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/;
     const value = $('#birthday').val();
@@ -46,15 +51,13 @@ function birthdayCheck() {
     return emptyAndPatternCheck(value, $birthdayMsg, pattern, '생일을 입력하세요', '정확한 생일을 입력하세요');
 }
 
-// 이메일 검증 함수
 function emailCheck() {
-    const pattern = /^[0-9a-zA-Z]([-.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+    const pattern = /^[0-9a-zA-Z]([--.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([--.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
     const value = $('#email').val();
     const $emailMsg = $('#email-msg');
     return emptyAndPatternCheck(value, $emailMsg, pattern, '이메일을 입력하세요', '정확한 이메일을 입력하세요');
 }
 
-// 비밀번호 검증 함수
 function passwordCheck(id) {
     const pattern = /^[A-Za-z0-9]{6,10}$/;
     const value = $('#' + id).val();
@@ -62,7 +65,6 @@ function passwordCheck(id) {
     return emptyAndPatternCheck(value, $passwordMsg, pattern, '비밀번호를 입력하세요', '비밀번호는 영숫자 6~10자입니다');
 }
 
-// 비밀번호 재확인 검증 함수
 function password2Check() {
     const password = $('#new-password').val();
     const password2 = $('#password2').val();
@@ -78,7 +80,6 @@ function password2Check() {
     return true;
 }
 
-// 아이디 검증 함수
 async function usernameCheck(availableCheck = true) {
     const pattern = /^[a-z0-9]{6,10}$/;
     const value = $('#username').val();
@@ -98,9 +99,3 @@ async function usernameCheck(availableCheck = true) {
         return false;
     }
 }
-
-// 문서가 준비되었을 때 전화번호 입력 필드에 이벤트 리스너 추가
-$(document).ready(function() {
-    $('#phone').on('input', formatPhoneInput);
-});
-
