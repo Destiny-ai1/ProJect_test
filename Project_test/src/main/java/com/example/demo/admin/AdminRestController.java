1cf661fdb592ca3d9f187aa7a396c4ec713c3fc1
kiwon <bob3824@naver.com>
2024-12-17 11:00:43 +0900

    오더html 없는스크립트(에러유발 코드)제거 어드민매퍼교체 어드민레스트컨트롤러 권한삭제

--- Project_test/src/main/java/com/example/demo/admin/AdminRestController.java
+++ Project_test/src/main/java/com/example/demo/admin/AdminRestController.java
@@ -1,25 +1,21 @@
 package com.example.demo.admin;
 
-import org.springframework.security.access.prepost.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.*;
 
 @Controller
-@PreAuthorize("hasRole('admin')")
 public class AdminRestController {
 
-	@PreAuthorize("isAnonymous()")
+
 	@GetMapping("terms")
 	public ModelAndView terms() 	{
 		return new ModelAndView("terms");
 	}
-	@PreAuthorize("isAnonymous()")
 	@GetMapping("guide")
 	public ModelAndView guide() 	{
 		return new ModelAndView("guide");
 	}
-	@PreAuthorize("isAnonymous()")
 	@GetMapping("privacy")
 	public ModelAndView privacy() 	{
 		return new ModelAndView("privacy");
